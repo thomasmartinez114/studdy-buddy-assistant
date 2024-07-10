879188fe0dd79a14958d0872509c42c28488d344
Thomas Martinez <devtommie114@gmail.com>
2024-07-09 22:19:19 -0400

    run and threads

--- app.py
+++ app.py
@@ -33,3 +33,61 @@ assistant = client.beta.assistants.create(
     model=model,
     file_ids=[file_object.id]
 )
+
+# === Get the Assistant ID === #
+assis_id = assistant.id
+print(assis_id)
+
+# === Hardcoded IDs to be used once the first code run is done === #
+# thread_id = ''
+# assis_id = ''
+
+# === Step 3. Create a Thread === #
+message = "What is mining?"
+
+thread = client.beta.threads.create()
+thread_id = thread.id
+print(thread_id)
+
+# === Run the Assistant === #
+run = client.beta.threads.create(
+    thread_id=thread_id,
+    assistant_id=assis_id,
+    instructions="Please address the user as Bruce"
+)
+
+def wait_for_run_completion(client, thread_id, run_id, sleep_interval=5):
+    """
+    Waits for a run to complete and prints the elapsed time.:param client: The OpenAI client object.
+    :param thread_id: The ID of the thread.
+    :param run_id: The ID of the run.
+    :param sleep_interval: Time in seconds to wait between checks.
+    """
+    while True:
+        try:
+            run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+            if run.completed_at:
+                elapsed_time = run.completed_at - run.created_at
+                formatted_elapsed_time = time.strftime(
+                    "%H:%M:%S", time.gmtime(elapsed_time)
+                )
+                print(f"Run completed in {formatted_elapsed_time}")
+                logging.info(f"Run completed in {formatted_elapsed_time}")
+                # Get messages here once Run is completed!
+                messages = client.beta.threads.messages.list(thread_id=thread_id)
+                last_message = messages.data[0]
+                response = last_message.content[0].text.value
+                print(f"Assistant Response: {response}")
+                break
+        except Exception as e:
+            logging.error(f"An error occurred while retrieving the run: {e}")
+            break
+        logging.info("Waiting for run to complete...")
+        time.sleep(sleep_interval)
+
+# === Run it === #
+wait_for_run_completion(client=client, thread_id=thread_id, run_id=run.id)
+
+# === Check the Run Steps - LOGS ===
+run_steps = client.beta.threads.runs.steps.list(thread_id=thread_id, run_id=run.id)
+print(f"Run Steps --> {run_steps.data[0]}")
