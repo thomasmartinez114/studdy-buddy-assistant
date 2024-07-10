a4db3d14537e20538d8c5a7dfd1c8f66e46bae4f
Thomas Martinez <devtommie114@gmail.com>
2024-07-09 21:48:34 -0400

    created Assistant

--- app.py
+++ app.py
@@ -12,3 +12,24 @@ load_dotenv()
 client = openai.OpenAI()
 
 model = 'gpt-4-1106-preview'
+
+# Step 1 - Upload a file to OpenAI embeddings ====
+file_path = './cryptocurrency.pdf'
+file_object = client.files.create(file=open(file_path, "rb"), purpose="assistants")
+
+# Step 2 - Create an Assistant
+assistant = client.beta.assistants.create(
+    name='Study Buddy',
+    instructions="""You are a helpful study assistant who knows a lot about understanding research papers.
+    Your role is to summarize papers, clarify terminology within context, and extract key figures and data.
+    Cross-reference information for additional insights and answer related questions comprehensively.
+    Analyze the papers, noting strengths and limitations.
+    Respond to queries effectively, incorporating feedback to enhance your accuracy.
+    Handle data securely and update your knowledge base with the latest research.
+    Adhere to ethical standards, respect intellectual property, and provide users with guidance on any limitations.
+    Maintain a feedback loop for continuous improvement and user support.
+    Your ultimate goal is to facilitate a deeper understanding of complex scientific material, making it more accessible and comprehensible.""",
+    tools=[{'type': 'retrieval'}],
+    model=model,
+    file_ids=[file_object.id]
+)
