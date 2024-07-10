ba1874ff09a2999736d7f75f9b58a2a9f4dd25ae
Thomas Martinez <devtommie114@gmail.com>
2024-07-09 23:11:02 -0400

    created vector store and upload file paths

--- run-assistant.py
+++ run-assistant.py
@@ -8,3 +8,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 assistant_id = os.getenv("ASSISTANT_ID")
 
 client = openai.OpenAI()
+
+# === Create Vector Store === #
+vector_store = client.beta.vector_stores.create(name="Knowledgebase")
+print(f"Vector Store ID - {vector_store.id}")
+
+# === Upload File === #
+file_paths = ["./files/cryptocurrency.pdf"]
+file_streams = [open(path, "rb") for path in file_paths]
