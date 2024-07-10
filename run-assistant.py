670f1f7f1a0a629e1fa4ad9b73d3469efa0b8190
Thomas Martinez <devtommie114@gmail.com>
2024-07-09 23:22:48 -0400

    Add files to vector, check status, update Asst with vector store

--- run-assistant.py
+++ run-assistant.py
@@ -16,3 +16,19 @@ print(f"Vector Store ID - {vector_store.id}")
 # === Upload File === #
 file_paths = ["./files/cryptocurrency.pdf"]
 file_streams = [open(path, "rb") for path in file_paths]
+
+# === Add files to Vector Store === #
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+    vector_store_id=vector_store.id,
+    files=file_streams
+)
+
+# === Check the status of Files === #
+print(f"File Status {file_batch.status}")
+
+# === Update the Assistant with a Vector Store
+assistant = client.beta.assistants.update(
+    assistant_id=assistant_id,
+    tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}}
+)
+print("Assistant Updated with vector store!")
